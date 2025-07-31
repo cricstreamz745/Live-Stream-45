9e67ccee1e093af6b1483a6600b7dca6109f0742
cricstreamz745 <cricketstream745@gmail.com>
2025-07-31 21:53:09 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-ffmpeg -re -i "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" \
+ffmpeg -re -i "http://mxonlive.atwebpages.com/filex/index.php/247167.m3u8" \
 -map 0:v:0 -map 0:a:0 \
 -c:v libx264 -preset ultrafast -tune zerolatency \
 -c:a aac -f flv \
