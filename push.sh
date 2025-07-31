2bdaf9cee039f9c8bebafd40202969685d02dd20
cricstreamz745 <cricketstream745@gmail.com>
2025-07-31 20:27:27 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-ffmpeg -re -i "http://mxonlive.atwebpages.com/filex/index.php/247167.m3u8" \
-  -c:v copy -c:a copy -f flv \
-  "rtmps://mumbai.onestream.studio:19350/live/live_4139356_p33qkhlvp?auth=p_auth_4139356_6twfl9un7"
+INPUT="http://mxonlive.atwebpages.com/filex/index.php/247167.m3u8"
+OUTPUT="rtmps://mumbai.onestream.studio:19350/live/live_4139356_p33qkhlvp?auth=p_auth_4139356_6twfl9un7"
+
+ffmpeg -re -i "$INPUT" \
+  -map 0:v:0 -map 0:a? \
+  -c:v copy -c:a aac -b:a 128k \
+  -f flv "$OUTPUT"
