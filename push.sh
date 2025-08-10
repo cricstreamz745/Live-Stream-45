ab34fc0f45c6e065baeb527a5a7fe14a53c3eb02
cricstreamz745 <cricketstream745@gmail.com>
2025-08-10 14:56:14 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@ ffmpeg -re -i "http://ee0c5e74.akadatel.com/iptv/QRDWGTBMDHSDGK/19146/index.m3u8
 -map 0:v:0 -map 0:a:0 \
 -c:v libx264 -preset ultrafast -tune zerolatency \
 -c:a aac -f flv \
-"rtmps://mumbai.onestream.studio:19350/live/live_4139402_8blems6ny?auth=p_auth_4139402_8om1rd7bt"
+"rtmps://mumbai.onestream.studio:19350/live/live_4314166_k51w5082d?auth=p_auth_4314166_op55me4tt"
